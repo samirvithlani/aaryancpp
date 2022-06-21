df930011c88a99bda75a0ac31be84d3f1fcc02b1
samirvithlani <samir.vithlani83955@gmail.com>
2022-06-21 16:07:48 +0530

    udf vector

--- stl/vector/vect.cpp
+++ stl/vector/vect.cpp
@@ -9,12 +9,8 @@ class Product{
         string name;
         int price;
         int cat[3]={1,2,3};
-
-};
-int main(){
-
-            vector<Product> products;
-            
+        vector<Product> products;
+        void add(){
             Product p1;
 
             p1.name = "apple";
@@ -22,14 +18,24 @@ int main(){
             p1.cat[0]=1;
 
             products.push_back(p1);
-            for(Product p1: products){
+
+        }
+        void print(){
+
+             for(Product p1: products){
 
                     cout<<"***";
                 cout<<"\n name = "<<p1.name;
                 cout<<"\n price = "<<p1.price;
                 cout<<"\n cat = "<<p1.cat[0];
             }
+        }
 
+};
+int main(){
 
+                    Product p;
+                    p.add();          
+                    p.print();      
 
 }
